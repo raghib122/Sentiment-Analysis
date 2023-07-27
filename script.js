3c8c5e526d9dba6178d88d4c42cc6f3d4a6f4d25
raghib122 <125207689+raghib122@users.noreply.github.com>
2023-07-28 00:36:51 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,72 +1,39 @@
-document.addEventListener("DOMContentLoaded", function () {
-    const apiKey = 'n8EyKaqyzclwKOGRZhUKrA==33iTgW0O8IM1Vy8F';
-    const apiUrl = 'https://api.api-ninjas.com/v1/sentiment';
-  
-    document.getElementById('searchBtn').addEventListener('click', function() {
-      const text = document.getElementById('textInput').value.trim();
-      if (text === '') {
-        alert('Please enter your text.');
-        return;
-      }
-  
-      fetchSentiment(apiUrl, apiKey, text)
-        .then(displaySentiment)
-        .catch(handleError);
-    });
-  
-    function fetchSentiment(url, apiKey, text) {
-      const headers = new Headers({
-        'X-Api-Key': apiKey,
-        'Content-Type': 'application/json'
-      });
-  
-      const requestOptions = {
-        method: 'GET',
-        headers: headers
-      };
-  
-      return fetch(`${url}?text=${encodeURIComponent(text)}`, requestOptions)
-        .then(response => {
-          if (!response.ok) {
-            throw new Error('Network response was not ok');
-          }
-          return response.json();
-        });
-    }
-  
-    function displaySentiment(data) {
-      const resultContainer = document.getElementById('resultContainer');
-      resultContainer.innerHTML = '';
-  
-      const score = data.score;
-      const sentiment = data.sentiment;
-      const scoreElement = document.createElement('p');
-      const sentimentElement = document.createElement('p');
-  
-      scoreElement.textContent = `Sentiment Score: ${score}`;
-      sentimentElement.textContent = `Sentiment: ${sentiment}`;
-  
-      resultContainer.appendChild(scoreElement);
-      resultContainer.appendChild(sentimentElement);
+document.getElementById("searchBtn").addEventListener("click", function () {
+  const text = document.getElementById("textInput").value;
+  if (text.length > 0) {
+    const apiKey = "n8EyKaqyzclwKOGRZhUKrA==33iTgW0O8IM1Vy8F";
+    const apiUrl = `https://api.api-ninjas.com/v1/sentiment?text=${encodeURIComponent(text)}`;
 
-      //Change bg image
-      const bodyElement = document.body;
-      bodyElement.className = '';
-      if (sentiment === 'NEUTRAL') {
-        bodyElement.classList.add('neutral-bg');
-      } else if (sentiment === 'POSITIVE') {
-        bodyElement.classList.add('positive-bg');
-      } else if (sentiment === 'WEAK_NEGATIVE') {
-        bodyElement.classList.add('weak-negative-bg');
-      } else {
-        bodyElement.classList.add('default-bg');
-      }
-  
-    }
-  
-    function handleError(error) {
-      console.error('An error occurred: ', error);
-      alert('An error occurred. Please try again later.');
-    }
-  });
-  
+    fetch(apiUrl, {
+      headers: { "X-Api-Key": apiKey },
+    })
+      .then((response) => response.json())
+      .then((result) => {
+        document.getElementById("resultText").textContent = result.text;
+        document.getElementById("resultScore").textContent = `Sentiment: ${result.sentiment} (${result.score.toFixed(3)})`;
+
+        
+        const body = document.body;
+        if (result.sentiment === "WEAK_NEGATIVE") {
+          body.style.backgroundColor = "black"; 
+        } else if (result.sentiment === "POSITIVE") {
+          body.style.backgroundColor = "#0093E9"; // Light green for positive
+        } else if (result.sentiment === "NEUTRAL") {
+          body.style.backgroundColor = "#FF6A88"; // Light gray for neutral
+        } else {
+          body.style.backgroundColor = "#FFFFFF"; 
+        }
+
+        document.getElementById("resultContainer").style.backgroundColor = ""; 
+        document.getElementById("resultContainer").style.display = "block";
+      })
+      .catch((error) => {
+        console.error("Error: ", error);
+        document.getElementById("resultText").textContent = "An error occurred.";
+        document.getElementById("resultScore").textContent = "";
+        document.body.style.backgroundColor = "#FFFFFF"; 
+        document.getElementById("resultContainer").style.backgroundColor = ""; // Reset result container background color
+        document.getElementById("resultContainer").style.display = "block";
+      });
+  }
+});
